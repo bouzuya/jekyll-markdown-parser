1074d4496b335ffd82caf1d40c80bb1f26cb6281
bouzuya <m@bouzuya.net>
2017-08-27 13:22:55 +0900

    beater@5.0.1

--- test/index.ts
+++ test/index.ts
@@ -1,4 +1,4 @@
-import beater from 'beater';
+import { Test, run, test } from 'beater';
 import * as assert from 'power-assert';
 
 import {
@@ -8,79 +8,81 @@ import {
   separate
 } from '../src/';
 
-const { test } = beater();
+const tests1: Test[] = [
+  test('compileMarkdown', () => {
+    const markdown = 'This is my first entry.';
+    const html = compileMarkdown(markdown);
+    assert(html === '<p>This is my first entry.</p>\n');
+  }),
 
-test('compileMarkdown', () => {
-  const markdown = 'This is my first entry.';
-  const html = compileMarkdown(markdown);
-  assert(html === '<p>This is my first entry.</p>\n');
-});
+  test('parse', () => {
+    const yaml = [
+      'layout: post',
+      'title: Hello Jekyll',
+    ].map((s) => s + '\n').join('');
+    const markdown = [
+      'This is my first entry.'
+    ].map((s) => s + '\n').join('');
+    const jekyllMarkdown = `---\n${yaml}---\n${markdown}`;
+    const parsed = parse(jekyllMarkdown);
+    assert.deepEqual(
+      parsed,
+      {
+        html: '<p>This is my first entry.</p>\n',
+        markdown,
+        parsedYaml: {
+          layout: 'post',
+          title: 'Hello Jekyll'
+        },
+        yaml
+      }
+    );
+  }),
 
-test('parse', () => {
-  const yaml = [
-    'layout: post',
-    'title: Hello Jekyll',
-  ].map((s) => s + '\n').join('');
-  const markdown = [
-    'This is my first entry.'
-  ].map((s) => s + '\n').join('');
-  const jekyllMarkdown = `---\n${yaml}---\n${markdown}`;
-  const parsed = parse(jekyllMarkdown);
-  assert.deepEqual(
-    parsed,
-    {
-      html: '<p>This is my first entry.</p>\n',
-      markdown,
-      parsedYaml: {
+  test('parseYaml', () => {
+    const yaml = [
+      'layout: post',
+      'title: Hello Jekyll',
+    ].join('\n');
+    const parsed = parseYaml(yaml);
+    assert.deepEqual(
+      parsed,
+      {
         layout: 'post',
         title: 'Hello Jekyll'
-      },
-      yaml
-    }
-  );
-});
+      }
+    );
+  }),
 
-test('parseYaml', () => {
-  const yaml = [
-    'layout: post',
-    'title: Hello Jekyll',
-  ].join('\n');
-  const parsed = parseYaml(yaml);
-  assert.deepEqual(
-    parsed,
-    {
-      layout: 'post',
-      title: 'Hello Jekyll'
-    }
-  );
-});
+  test('separate', () => {
+    const yaml = [
+      'layout: post',
+      'title: Hello Jekyll',
+    ].map((s) => s + '\n').join('');
+    const markdown = [
+      'This is my first entry.'
+    ].map((s) => s + '\n').join('');
+    const jekyllMarkdown = `---\n${yaml}---\n${markdown}`;
+    const separated = separate(jekyllMarkdown);
+    assert.deepEqual(separated, { markdown, yaml });
+  }),
 
-test('separate', () => {
-  const yaml = [
-    'layout: post',
-    'title: Hello Jekyll',
-  ].map((s) => s + '\n').join('');
-  const markdown = [
-    'This is my first entry.'
-  ].map((s) => s + '\n').join('');
-  const jekyllMarkdown = `---\n${yaml}---\n${markdown}`;
-  const separated = separate(jekyllMarkdown);
-  assert.deepEqual(separated, { markdown, yaml });
-});
+  test('separate 2', () => {
+    const yaml = [
+      'layout: post',
+      'title: Hello Jekyll',
+    ].map((s) => s + '\n').join('');
+    const markdown = [
+      'This is my first entry.',
+      '---',
+      'Hello',
+      '---',
+      'World'
+    ].map((s) => s + '\n').join('');
+    const jekyllMarkdown = `---\n${yaml}---\n${markdown}`;
+    const separated = separate(jekyllMarkdown);
+    assert.deepEqual(separated, { markdown, yaml });
+  })
+];
 
-test('separate 2', () => {
-  const yaml = [
-    'layout: post',
-    'title: Hello Jekyll',
-  ].map((s) => s + '\n').join('');
-  const markdown = [
-    'This is my first entry.',
-    '---',
-    'Hello',
-    '---',
-    'World'
-  ].map((s) => s + '\n').join('');
-  const jekyllMarkdown = `---\n${yaml}---\n${markdown}`;
-  const separated = separate(jekyllMarkdown);
-  assert.deepEqual(separated, { markdown, yaml });
-});
+run(tests1).catch(() => process.exit(1));
